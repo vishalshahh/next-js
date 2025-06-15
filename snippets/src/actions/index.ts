a263503d599d0ad50fd7284b55a44770f46858ba
Visha193 <Vishal12kl@gmail.com>
2025-06-15 12:53:05 +0530

    feat: add error handling properly in snippet project

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -1,19 +1,51 @@
-"use server"
+"use server";
 
-import { prisma } from "@/lib/prisma"
+import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 
-export const saveSnippet = async (id:number, code:string) => {
+export const saveSnippet = async (id: number, code: string) => {
   await prisma.snippet.update({
     where: { id },
     data: { code },
   });
   redirect(`/snippet/${id}`);
-}
+};
 
-export const deleteSnippet = async (id:number) => {
+export const deleteSnippet = async (id: number) => {
   await prisma.snippet.delete({
     where: { id },
   });
+  redirect("/");
+};
+
+export async function createSnipppet(
+  prevState: { message: string },
+  formData: FormData
+) {
+  try {
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 4) {
+      return {
+        message: "Title is required and must be longer than 4 characters",
+      };
+    }
+    if (typeof code !== "string" || code.length < 8) {
+      return {
+        message: "Code is required and must be longer than 8 characters",
+      };
+    }
+
+    await prisma.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (error: any) {
+    return { message: error.message };
+  }
+
   redirect("/");
 }
--- snippets/src/app/snippet/new/page.tsx
+++ snippets/src/app/snippet/new/page.tsx
@@ -1,31 +1,18 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { prisma } from "@/lib/prisma";
-import { redirect } from "next/navigation";
-import React from "react";
+import { useActionState } from "react";
+import * as actions from "@/actions";
 
 const CreateSnippetPage = () => {
-
-  async function createSnippet(formData:FormData){
-    "use server" //use server action
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    const snippet = await prisma.snippet.create({
-      data: {
-        title,
-        code
-      }
-    });
-    console.log("Snippet created:", snippet);
-
-    redirect("/");
-  }
+  const [formStateData, action] = useActionState(actions.createSnipppet, {
+    message: "",
+  });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <div>
         <Label className="mt-4">Title</Label>
         <Input type="text" name="title" id="title" />
@@ -34,6 +21,11 @@ const CreateSnippetPage = () => {
         <Label className="mt-4">Code</Label>
         <Textarea name="code" id="code" />
       </div>
+      {formStateData.message && (
+        <div className="p-2 bg-red-300 border-2 border-red-600 mt-2">
+          {formStateData.message}
+        </div>
+      )}
       <Button type="submit" className="mt-4">
         Create Snippet
       </Button>
